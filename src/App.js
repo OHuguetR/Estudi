81d1ef6f27afc64ae6d7426a2b288c097e169550
David Camacho <camachocateura1997@gmail.com>
2022-03-17 22:08:33 +0100

    Fixed formatting and routing errors

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import React from "react";
-import {Route} from "react-router-dom";
-import {Routes} from "react-router-dom";
-/* import Home from "./pages/Home";
-import NewMeetupPage from "./pages/NewMeetup";
-import FavouritesPage from "./pages/Favourites"; */
+import { Route } from "react-router-dom";
+import { Routes } from "react-router-dom";
 
-function App(){
-    return (
-          {/* <Routes> */}
-      <Route exact path="/">
-      {/* <Home/> */}<p>hola</p>
-      </Route>
-    {/* <Route exact path='/NewMeetup'>
-      <NewMeetupPage/>
-    </Route>
-    <Route exact path='/Favourites'>
-      <FavouritesPage/>
-    </Route> */}
-    {/* </Routes> */}
-    );
+import Home from "./pages/Home";
+import NewMeetupPage from "./pages/NewMeetup";
+import FavouritesPage from "./pages/Favourites";
+
+function App() {
+  return (
+    <Routes>
+      <Route exact path="/" element={<Home />} />
+      <Route exact path='/NewMeetup' element={<NewMeetupPage />} />
+      <Route exact path='/Favourites' element={<FavouritesPage />} />
+    </Routes>
+  );
 }
 export default App;
